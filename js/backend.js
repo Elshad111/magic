5210d8c3b208d749f22e8caa26583741b8c3405d
Elshad <safarov.elshad007@gmail.com>
2019-02-06 12:39:54 +0500

    Added upload method

--- js/backend.js
+++ js/backend.js
@@ -1,27 +1,40 @@
 'use strict';
 (function(){
-	window.load = function(url, onSuccess, onError){
-		var xhr = new XMLHttpRequest();
-		xhr.responseType = 'json';
-		xhr.addEventListener('load', function(){
-			if(xhr.status === 200){
+	window.backend = {
+		load: function(url, onSuccess, onError){
+			var xhr = new XMLHttpRequest();
+			xhr.responseType = 'json';
+			xhr.addEventListener('load', function(){
+				if(xhr.status === 200){
+					onSuccess(xhr.response);
+				}else{
+					onError("Статус ответа : " + xhr.status + " " + xhr.statusText);
+				}
+			});
+			xhr.addEventListener('error', function(){
+				onError('Произошла ошибка соединения');
+			});
+			xhr.addEventListener('timeout', function(){
+				onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+			});
+			xhr.timeout = 10000;
+
+			xhr.open('GET', url);
+			xhr.send();
+		},
+		upload: function(url, data, onSuccess){
+			var xhr = new XMLHttpRequest();
+			xhr.responseType = 'json';
+
+			xhr.addEventListener('load', function(){
 				onSuccess(xhr.response);
-			}else{
-				onError("Статус ответа : " + xhr.status + " " + xhr.statusText);
-			}
-		});
-		xhr.addEventListener('error', function(){
-			onError('Произошла ошибка соединения');
-		});
-		xhr.addEventListener('timeout', function(){
-			onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-		});
-		xhr.timeout = 10000;
+			});
 
-		xhr.open('GET', url);
-		xhr.send();
-
-	}
+			xhr.open('POST', url);
+			xhr.send(data);
+		}
+	};
+	
 })();
 
 (function(){
@@ -48,5 +61,12 @@
 		setupSimilarList.appendChild(fragment);
 		window.setup.setup.querySelector('.setup-similar').classList.remove('hidden');
 	};
-	window.load('https://js.dump.academy/code-and-magick/data', onSuccess, onError);
+	var form = window.setup.setup.querySelector('.setup-wizard-form');
+	form.addEventListener('submit', function(evt){
+		window.backend.upload('https://js.dump.academy/code-and-magick', new FormData(form), function(){
+			window.setup.setup.classList.add('hidden');
+		});
+		evt.preventDefault();
+	});
+	window.backend.load('https://js.dump.academy/code-and-magick/data', onSuccess, onError);
 })();
