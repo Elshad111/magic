487ad05fd541377e905a901c1531c49638d7fe52
Elshad <safarov.elshad007@gmail.com>
2019-02-06 15:15:39 +0500

    Added errorHandler method

--- js/backend.js
+++ js/backend.js
@@ -5,23 +5,27 @@
 			var xhr = new XMLHttpRequest();
 			xhr.responseType = 'json';
 			xhr.addEventListener('load', function(){
-				if(xhr.status === 200){
+				if(xhr.status == 200){
 					onSuccess(xhr.response);
 				}else{
 					onError("Статус ответа : " + xhr.status + " " + xhr.statusText);
 				}
 			});
+
 			xhr.addEventListener('error', function(){
 				onError('Произошла ошибка соединения');
 			});
+
 			xhr.addEventListener('timeout', function(){
 				onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
 			});
-			xhr.timeout = 10000;
+
+			xhr.timeout = 10000; // 10 seconds
 
 			xhr.open('GET', url);
 			xhr.send();
 		},
+
 		upload: function(url, data, onSuccess){
 			var xhr = new XMLHttpRequest();
 			xhr.responseType = 'json';
@@ -34,7 +38,6 @@
 			xhr.send(data);
 		}
 	};
-	
 })();
 
 (function(){
@@ -42,25 +45,38 @@
 		.content.querySelector('.setup-similar-item');
 	var setupSimilarList = window.setup.setup.querySelector('.setup-similar-list');
 
-	var renderWizard = function(wizard){
+	var successHandler = function(wizard){
 		var getWizardElement = similarWizardTemplate.cloneNode(true);
 		getWizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
 		getWizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
 		getWizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 		return getWizardElement;
 	}
-	var onError = function(message){
-		console.log(message);
-	};
+
+	var renderError = function(errorMessage){
+		var node = document.createElement('div');
+		node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red';
+		node.style.position = 'absolute';
+		node.style.left = 0;
+		node.style.right = 0;
+		node.style.fontSize = '30px';
+		node.textContent = errorMessage;
+		document.body.insertAdjacentElement('afterend', node);
+	}
 
 	var onSuccess = function(data){
 		var fragment = document.createDocumentFragment();
 		for(var i = 0; i < 4; i++){
-			fragment.appendChild(renderWizard(data[i]));
+			fragment.appendChild(successHandler(data[i]))
 		}
 		setupSimilarList.appendChild(fragment);
 		window.setup.setup.querySelector('.setup-similar').classList.remove('hidden');
 	};
+
+	var onError = function(message){
+		console.log(message);
+	};
+
 	var form = window.setup.setup.querySelector('.setup-wizard-form');
 	form.addEventListener('submit', function(evt){
 		window.backend.upload('https://js.dump.academy/code-and-magick', new FormData(form), function(){
@@ -68,5 +84,5 @@
 		});
 		evt.preventDefault();
 	});
-	window.backend.load('https://js.dump.academy/code-and-magick/data', onSuccess, onError);
+	window.backend.load('https://js.dump.academy/code-and-magick/data', onSuccess, renderError);
 })();
