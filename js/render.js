ac61a31df574d55be4f4cd8e3f863f7370af098e
Elshad <safarov.elshad007@gmail.com>
2019-02-12 12:14:21 +0500

    The render method has been added to the similar.js file

--- js/render.js
+++ js/render.js
@@ -1,7 +1,7 @@
 "use strict";
 (function(){
 	var wizardTemplate = document.quereySelector("#similar-wizard-template");
-	
+
 	var renderWizard = function(wizards){
 		var element = wizardTemplate.content.cloneNode(true);
 
@@ -11,5 +11,18 @@
 		element.querySelector('.setup-similar-label').textContent =  wizard.name;
 
 		return element;
+	};
+
+	var similar = document.querySelector('.setup-similar');
+	var similarList = document.querySelector('setup-similar-list');
+
+	window.render = function(data){
+		var takeNumber = data.length > 4 ? 4 : data.length;
+		similarList.innerHtml = "";
+		for(var i = 0; i < takeNumber; i++){
+			similar.appendChild(renderWizard(data[i]));
+		}
+
+		similar.classList.remove('hidden');
 	};
 })();
