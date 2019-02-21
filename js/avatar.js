741f7e8434320d4144e731374d39c4faeb71caec
Elshad <safarov.elshad007@gmail.com>
2019-02-21 23:15:48 +0500

    Modified avatar.js file

--- js/avatar.js
+++ js/avatar.js
@@ -1,13 +1,15 @@
 'use strict';
 (function(){
-	var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+	var FILE_TYPES = ['jpeg', 'jpg', 'png', 'gif'];
 
 	var fileChooser = document.querySelector('.upload input[type=file]');
 	var preview = document.querySelector('.setup-user-pic');
+	console.log(preview.name);
 
 	fileChooser.addEventListener('change', function(){
 		var file = fileChooser.files[0];
 		var fileName = file.name.toLowerCase();
+		//console.log(file.name);
 
 		var matches = FILE_TYPES.some(function(it){
 			return fileName.endsWith(it);
@@ -15,11 +17,13 @@
 
 		if(matches){
 			var reader = new FileReader();
-
 			reader.addEventListener('load', function(){
 				preview.src = reader.result;
 			});
+
 			reader.readAsDataURL(file);
+		}else{
+			alert('Неверный формат файла!');
 		}
 	});
 })();
